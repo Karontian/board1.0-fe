e8ce344a962bd149844faa15e23d79cb4045e85d
Karontian <karontianpch@gmail.com>
2024-05-08 18:41:35 -0600

    session start added board css

--- src/components/boardAdmin.jsx
+++ src/components/boardAdmin.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-
 import './boardAdmin.css'
 
 const BoardAdmin  = () =>{
@@ -12,9 +11,11 @@ const BoardAdmin  = () =>{
     const [address, setAddress] = useState('');
     const [mcNumber, setMcNumber] = useState('');
     const [dotNumber, setDotNumber] = useState('');
-    const [einNumber, setEinNumber] = useState('')
-    const [currentClients, setCurrentClients] = useState('')
+    const [einNumber, setEinNumber] = useState('');
+    const [currentClients, setCurrentClients] = useState('');
+    const [equipment, setEquipment] = useState([])
 
+    
 
     useEffect(() => {
         getClients()
@@ -97,14 +98,43 @@ const BoardAdmin  = () =>{
             console.log(error)
         }
     }
-    
+
+    const addEquipment = (e) => {
+        const {name} = e.target;
+        e.target.checked ? 
+        (() => {
+            console.log('CHECKED');
+            let amount = prompt(`How many ${name} units? `);
+            setEquipment(prevEquipment => [...prevEquipment, { name, amount }]);
+        })() : 
+        (() => {
+            console.log('UNCHECK');
+            setEquipment(prevEquipment => prevEquipment.filter(item => item.name !== name));
+
+        })();
+    }   
+
+    const onDriverAdd = async(e) =>{
+        e.preventDefault()
+        console.log('DRIVER ADD', e.target)
+        const {name, value} = e.target.name
+        console.log(name)
+        // try {
+        //     let driverPost = await axios.post('http://localhost:3001/driverAdd')
+        //     console.log(driverPost)
+
+        // } catch (err) {
+        //     console.log(err)
+        // }
+    }
+
 
 
     return (
         <div className="mainContent-boardAdmin">
             <div className="newCompanyForm-boardAdmin">
                 <form onSubmit={(e)=>onCompanySubmit(e)}>
-                <h1>New Company Add:</h1>
+                <h1>Add a new copany:</h1>
 
                     <label htmlFor="companyName">Company Name: </label>
                     <input type="text" name="companyName" onChange={onChange}/>
@@ -134,6 +164,106 @@ const BoardAdmin  = () =>{
                     
                 </form>
             </div>
+
+            <div className='newDriverForm-boardAdmin'>
+                   <form  onSubmit={(e)=>onDriverAdd(e)}>
+                       <h3>Add a new Driver</h3>
+                         <label htmlFor="client">Select a Client:</label>
+                         <select name="client" id="client">
+                            <option value="default"></option>
+                             {Array.from(currentClients).map((company, index) => {
+                                return <option key={index} value={company.companyName}>{company.companyName}</option>
+                            })}    
+
+                         </select>
+                        <div className='newDriverForm-equipmentCheckBoxes'>
+                            <div className='equipmentCheckBoxes-trailerType'>
+                                <span>Trailer Type:</span>
+
+                                <div className='trailerType-fb'>
+                                    
+                                    <label htmlFor="fb48">F48</label>
+                                    <input type="checkBox" id='fb48' name='fb48' />
+
+                                    <label htmlFor="fb53">F53</label>
+                                    <input type="checkBox" id='fb53' name='fb53' />
+
+                                </div>
+                                <div className='trailerType-van'>
+                                    <label htmlFor="van48">V48</label>
+                                    <input type="checkBox" id='van48' name='van48' />
+
+                                    <label htmlFor="van53">V53</label>
+                                    <input type="checkBox" id='van53' name='van53' />
+
+
+                                </div>
+                                <div className='trailerType-reefer'>
+                                    <label htmlFor="r48">R48</label>
+                                    <input type="checkBox" id='r48' name='r48' />
+
+                                    <label htmlFor="r53">R53</label>
+                                    <input type="checkBox" id='r53' name='r53' />
+
+
+                                </div>
+
+
+                            </div>
+                            <div className='equipmentCheckBoxes-equipment'>
+                                <span>Equipment:</span>
+                                <div className='equipment-tarps'>
+                                    <span>Tarps:   </span>
+
+                                            <label htmlFor="tarps8ft">8FT</label>
+                                            <input type="checkBox" id='tarps6ft' name='tarps8ft' onClick={(e)=>addEquipment(e)} />
+
+                                            <label htmlFor="tarps6ft">6FT</label>
+                                            <input type="checkBox" id='tarps6ft' name='tarps6ft' onClick={(e)=>addEquipment(e)} />
+
+                                            <label htmlFor="tarps4ft">4FT</label>
+                                            <input type="checkBox" id='tarps4ft' name='tarps4ft' onClick={(e)=>addEquipment(e)} />
+
+
+                                </div>
+
+                                <div className='equipment-chains'>
+                                <span>Securing:   </span>
+
+                                            <label htmlFor="chains">Chains</label>
+                                            <input type="checkBox" id='chains' name='chains' onClick={(e)=>addEquipment(e)} />
+
+                                            <label htmlFor="binders">Binders</label>
+                                            <input type="checkBox" id='binders' name='binders' onClick={(e)=>addEquipment(e)} />
+
+                                </div>
+
+                                <div className='equipment-misc'>
+                                <span>Miscelanious: </span>
+
+                                        <label htmlFor="pipeStakes">Pipe Stakes</label>
+                                        <input type="checkBox" id='pipeStakes' name='pipeStakes' onClick={(e)=>addEquipment(e)}/>
+
+                                        <label htmlFor="dunnage">Dunnage</label>
+                                        <input type="checkBox" id='dunnage' name='dunnage' onClick={(e)=>addEquipment(e)} />
+
+
+                                </div>
+                                
+                            </div>
+                        </div>    
+                    
+                        <label htmlFor="driverName">Driver Name:</label>
+                        <input type="text" name='driverName'/>
+
+                        <label htmlFor="driverPhoneNumber">Driver Phone#:</label>
+                        <input type="text" name='driverPhoneNumber' />
+                        
+                        <button type='submit'>Add+</button>
+
+                   </form>
+            </div>
+
             <div className='companyBoard-boardAdmin'>
                 <h1>Current Companies</h1>
                 <table>
@@ -166,7 +296,6 @@ const BoardAdmin  = () =>{
                                         <td>{company.dotNumber}</td>
                                         <td>{company.einNumber}</td>
                                         <td>
-                                            <button type='button'>Add Driver</button>
                                             <button>Edit Cpmpany</button>
                                             <button>Delete Company</button>
                                         </td>
@@ -188,6 +317,9 @@ const BoardAdmin  = () =>{
                                                     </tr>
                                                 </thead>
                                                 <tbody>
+                                                    <tr>
+                                                        <td>Driver elements here</td>
+                                                    </tr>
                                                 </tbody>
                                             </table>
                                         </td>
@@ -198,7 +330,9 @@ const BoardAdmin  = () =>{
                     </tbody>
                     <tfoot></tfoot>
                 </table>
+       
             </div>
+           
         
         </div>
     
