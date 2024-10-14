812f079c28ae5667210470bc1b7880b60d3f3413
Karontian <karontianpch@gmail.com>
2024-10-14 17:12:12 -0600

    changes in CSS of boardAdmin component

--- src/components/boardAdmin.jsx
+++ src/components/boardAdmin.jsx
@@ -821,7 +821,7 @@ const BoardAdmin  = () =>{
                 </div>
             <div className="newCompanyForm-boardAdmin">
                 <form onSubmit={(e)=>onCompanySubmit(e)}>
-                <h1>Add a new copany:</h1>
+                <h1>01: Add a new copany:</h1>
 
                     <label htmlFor="companyName">Company Name: </label>
                     <input type="text" 
@@ -898,7 +898,31 @@ const BoardAdmin  = () =>{
                             })}    
 
                          </select>
-                        <div className='newDriverForm-equipmentForm'>
+                         <div className='newDriverForm-driverForm'>
+                            <label htmlFor="driverName">Driver Name:</label>
+                            <input type="text" name='driverName'  onChange={e =>setDriverName(e.target.value)} required placeholder='Driver Name'/>
+
+                            <label htmlFor="driverPhoneNumber">Driver Phone#:</label>
+                            <input type="text" name='driverPhoneNumber' onChange={e =>setDriverPhone(e.target.value) } required placeholder='xxx - xxx - xxxx' />
+                            
+                            <label htmlFor="currentLocation">Current Location:</label>
+                            <input type="text" name='currentLocation' onChange={e => setCurrentLocation(e.target.value)} required placeholder='Current location' />
+                            
+                            <label htmlFor="inactiveDriver">Inactive</label>
+                            <input type="checkBox" name="inactiveDriver" id="inactiveDriver"  onChange={e => setDriverStatus(e.target.checked ? 'inactive' : null)} />   
+                            
+                            <label htmlFor="availableDate">Available Date:</label>                                                        
+                            <input type="date" name="availableDate" id="" onChange={e => onDateSetup(e, e.target.value)} disabled={driverStatus} required />
+          
+                            <label htmlFor="assignedDispatcher">Assigned Dispatcher</label>
+                            <select id="assignedDispatcher" name="assignedDispatcher" onChange={e => setAssignedDispatcher(e.target.value)}>
+                                <option value="default">Select a dispatcher:</option>
+                                <option value="dispatcher1">dispatcher1</option>
+                                <option value="dispatcher2">dispatcher2</option>
+                                <option value="dispatcher3">dispatcher3</option>
+                            </select>     
+
+                            <div className='newDriverForm-equipmentForm'>
                                 <div className='equipmentForm-trailerType'>
                                 <strong>Trailer Type:</strong>
 
@@ -1062,36 +1086,178 @@ const BoardAdmin  = () =>{
                                 </div>    
                                 
                             </div>
-                        </div>    
-                        <div className='newDriverForm-driverForm'>
-                            <label htmlFor="driverName">Driver Name:</label>
-                            <input type="text" name='driverName'  onChange={e =>setDriverName(e.target.value)} required placeholder='Driver Name'/>
-
-                            <label htmlFor="driverPhoneNumber">Driver Phone#:</label>
-                            <input type="text" name='driverPhoneNumber' onChange={e =>setDriverPhone(e.target.value) } required placeholder='xxx - xxx - xxxx' />
-                            
-                            <label htmlFor="currentLocation">Current Location:</label>
-                            <input type="text" name='currentLocation' onChange={e => setCurrentLocation(e.target.value)} required placeholder='Current location' />
-                            
-                            <label htmlFor="inactiveDriver">Inactive</label>
-                            <input type="checkBox" name="inactiveDriver" id="inactiveDriver"  onChange={e => setDriverStatus(e.target.checked ? 'inactive' : null)} />   
-                            
-                            <label htmlFor="availableDate">Available Date:</label>                                                        
-                            <input type="date" name="availableDate" id="" onChange={e => onDateSetup(e, e.target.value)} disabled={driverStatus} required />
-          
-                            <label htmlFor="assignedDispatcher">Assigned Dispatcher</label>
-                            <select id="assignedDispatcher" name="assignedDispatcher" onChange={e => setAssignedDispatcher(e.target.value)}>
-                                <option value="default">Select a dispatcher:</option>
-                                <option value="dispatcher1">dispatcher1</option>
-                                <option value="dispatcher2">dispatcher2</option>
-                                <option value="dispatcher3">dispatcher3</option>
-                            </select>                            
-                                                                    
+                        </div>              
                             <button type='submit' >Save</button>
                             <button type='button' onClick={(e)=>onDriverAddCancel(e)}>Cancel</button>
                             {/* <button type='button' onClick={(e)=>onAddAnotherDriver(e)} disabled={addAnotherDriver === false}> Add another Driver</button> */}
 
-                        </div>         
+                        </div> 
+                        {/* <div className='newDriverForm-equipmentForm'>
+                                <div className='equipmentForm-trailerType'>
+                                <strong>Trailer Type:</strong>
+
+                                <div className='trailerType-fb'>
+                                    <input type="text" className='equipmentOption-trailerType' name='fb48-ammount' onChange={e => setFb48Amount(e.target.value)} value={fb48Amount} />
+                                    <label htmlFor="fb48-ammount">xFB48 default?</label>
+                                    <input type="checkbox" id='fb48' name='default-fb48' checked={checkedTrailer === 'default-fb48'} onChange={() => handleCheckboxChange('default-fb48') } />
+
+                                    <input type="text" className='equipmentOption-trailerType' name='fb53-ammount' onChange={e => setFb53Amount(e.target.value)} value={fb53Amount} />
+                                    <label htmlFor="fb53-ammount">xFB53 default?</label>
+                                    <input type="checkbox" id='fb53' name='default-fb53' checked={checkedTrailer === 'default-fb53'} onChange={() => handleCheckboxChange('default-fb53')} />
+                                </div>
+
+                                <div className='trailerType-van'>
+                                    <input type="text" className='equipmentOption-trailerType' name='van48-ammount' onChange={e => setVan48Amount(e.target.value)} value={van48Amount}/>
+                                    <label htmlFor="van48-ammount">xV48 default?</label>
+                                    <input type="checkbox" id='van48' name='default-van48' checked={checkedTrailer === 'default-van48'} onChange={() => handleCheckboxChange('default-van48')} />
+
+                                    <input type="text" className='equipmentOption-trailerType' name='van53-ammount' onChange={e => setVan53Amount(e.target.value)}  value={van53Amount}/>
+                                    <label htmlFor="van53-ammount">xV53 default?</label>
+                                    <input type="checkbox" id='van53' name='default-van53' checked={checkedTrailer === 'default-van53'} onChange={() => handleCheckboxChange('default-van53')} />
+                                </div>
+
+                                <div className='trailerType-reefer'>
+                                    <input type="text" className='equipmentOption-trailerType' name='reefer48-ammount' onChange={e => setReefer48Amount(e.target.value)} value={reefer48Amount} />
+                                    <label htmlFor="reefer48-ammount">xR48 default?</label>
+                                    <input type="checkbox" id='reefer48' name='default-reefer48' checked={checkedTrailer === 'default-reefer48'} onChange={() => handleCheckboxChange('default-reefer48')} />
+
+                                    <input type="text" className='equipmentOption-trailerType' name='reefer53-ammount' onChange={e => setReefer53Amount(e.target.value)}  value={reefer53Amount}/>
+                                    <label htmlFor="reefer53-ammount">xR53 default?</label>
+                                    <input type="checkbox" id='reefer53' name='default-reefer53' checked={checkedTrailer === 'default-reefer53'} onChange={() => handleCheckboxChange('default-reefer53')} />
+                                </div>
+
+                                <div className='trailerType-other'>
+                                    <button type='button' onClick={() => setOtherTypeOfTrailerSelected(true)} disabled={otherTypeOfTrailerSelected === true}>+Other Type Trailer</button>
+
+                                    <div className='trailerType-other-form'>
+                                    {otherTypeOfTrailerSelected === true &&
+                                        <div className='other-trailerType'>
+                                        <h5>Add a new Other-Type trailer:</h5>
+
+                                        <input type="text" name='amount' className='other-trailerType-amount' ref={amountRef}  required/>
+                                        <label htmlFor="amount">x</label>
+
+                                        <label htmlFor="type">Type</label>
+                                        <input type="text" name='type' className='other-trailerType-type' ref={typeRef}  required/>
+                                        <br />
+
+                                        <label htmlFor="length">Length</label>
+                                        <input type="text" name='length' className='other-trailerType-length' ref={lengthRef}  required/>
+                                        <span>ft</span>
+
+                                        <label htmlFor="default">Default?</label>
+                                        <input type="checkbox" name='default' className='other-trailerType' ref={defaultRef} checked={checkedTrailer === 'default-other'} onChange={() => handleCheckboxChange('default-other')} />
+
+                                        <button type='button' onClick={(e) => onSaveOtherTypeTrailer(e)}>Save Trailers</button>
+                                        <button type='button' onClick={(e) => onCancelOtherTrailers(e)}>Cancel</button>
+                                        </div>
+                                    }
+                                    </div>
+
+                                    <div className='trailerType-other-grid'>
+                                    <h3>Other-Type Trailers added:</h3>
+                                    <table>
+                                        <thead>
+                                        <tr>
+                                            <th>Amount</th>
+                                            <th>Trailer Type</th>
+                                            <th>Trailer Length</th>
+                                            <th>Default?</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        {otherTypeTrailerArray.map((item, index) => (
+                                            editingIndex === index ? ( // EDITING MODE
+                                            <tr key={index}>
+                                                <td>
+                                                <label htmlFor="editedAmount"></label>
+                                                <input type="text" name='editedAmount' value={ottEditingAmount} onChange={(e) => setOttEditingAmount(e.target.value)} />
+                                                </td>
+                                                <td>
+                                                <label htmlFor="editedType"></label>
+                                                <input type="text" name='editedType' value={ottEditingType} onChange={(e) => setOttEditingType(e.target.value)} />
+                                                </td>
+                                                <td>
+                                                <label htmlFor="editedLength"></label>
+                                                <input type="text" name='editedLength' value={ottEditingLenght} onChange={(e) => setOttEditingLenght(e.target.value)} />
+                                                </td>
+                                                <td>
+                                                <input type="checkbox" name='editedDefault' checked={ottEditingDefault && checkedTrailer === 'editedDefault'  } onChange={(e) => (setOttEditingDefault(e.target.checked ), handleCheckboxChange('editedDefault'))} />
+                                                </td>
+                                                <td>
+                                                <button type='button' onClick={(e) => onOtherTrailerEditSave(e, index)}>Save</button>
+                                                <button type='button'>Cancel</button>
+                                                </td>
+                                            </tr>
+                                            ) : (
+                                            <tr key={index}>
+                                                <td>{item.amount}</td>
+                                                <td>{item.type}</td>
+                                                <td>{item.length}</td>
+                                                <td>
+                                                <label htmlFor="defaultTrailer"></label>
+                                                <input type="checkbox" name='defaultTrailer' checked={item.def} readOnly />
+                                                </td>
+                                                <td>
+                                                <button type='button' onClick={(e) => onOtherTrailerDelete(e, index)}>Delete</button>
+                                                <button type='button' onClick={(e) => onOtherTrailerEdit(e, index)}>Edit</button>
+                                                </td>
+                                            </tr>
+                                            )
+                                        ))}
+                                        </tbody>
+                                        <tfoot>
+                                        </tfoot>
+                                    </table>
+                                    </div>
+                                </div>
+                                </div>
+
+
+                            <div className='equipmentForm-equipment'>
+                                <div className='equipment-tarps'>
+                                <span>Equipment:</span>
+                                    <br />
+                                    <span>Tarps:   </span>
+                                            <br />
+                                            <input type="text" className='equipment-tarps8ft' name='equipment-tarps8ft' onChange={e => setTarps8ft(e.target.value)} />
+                                            <label htmlFor="equipment-tarps8ft">x 8FT Tarps</label>
+
+                                            <br />
+                                            <input type="text" className='equipment-tarps6ft' name='equipment-tarps6ft'  onChange={e => setTarps6ft(e.target.value)} />
+                                            <label htmlFor="equipment-tarps6ft">x 6FT Tarps</label>
+
+                                            <br />
+                                            <input type="text" className='equipment-tarps4ft' name='equipment-tarps4ft'  onChange={e => setTarps4ft(e.target.value)} />
+                                            <label htmlFor="equipment-tarps4ft">x 4FT Tarps</label>
+
+
+                                </div>
+
+                                <div className='equipment-sec'>
+                                <span>Securing:   </span>
+                                            <br />
+                                            <input type="text" className='equipment-chains' name='equipment-chains'  onChange={e => setChains(e.target.value)} />
+                                            <label htmlFor="equipment-chains">x Chains</label>
+
+                                            <br />
+                                            <input type="text" className='equipment-binders' name='equipment-binders'  onChange={e => setBinders(e.target.value)} />
+                                            <label htmlFor="equipment-binders">x Binders</label>
+
+                                            <br />
+                                            <input type="text" className='equipment-pipeStakes' name='equipment-pipeStakes'  onChange={e => setPipeStakes(e.target.value)} />
+                                            <label htmlFor="equipment-pipeStakes">x PipeStakes</label>
+
+                                            <br />
+                                            <input type="text" className='equipment-dunnage' name='equipment-dunnage'  onChange={e => setDunnage(e.target.value)} />
+                                            <label htmlFor="equipment-dunnage">x Dunnage</label>
+
+                                </div>    
+                                
+                            </div>
+                        </div>     */}
+                                
                    </form>
                    <ToastContainer />
 
